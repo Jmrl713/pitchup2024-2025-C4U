6909d1c5af26eb98a52f0e34f666c7a255a26786
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2024-10-28 03:50:56 +0000

    Updating to latest FlutterFlow output.

---
+++ lib/index.dart
@@ -0,0 +1,4 @@
+// Export pages
+export '/pages/log_in_page/log_in_page_widget.dart' show LogInPageWidget;
+export '/pages/get_started/get_started_widget.dart' show GetStartedWidget;
+export '/su_birthday/su_birthday_widget.dart' show SuBirthdayWidget;
